0f06fcc4dcf91f7be0d905b9f1e56c18e30061fd
Angel Santana <angel.d.santana00@gmail.com>
2022-03-14 15:01:31 -0500

    auto save 3

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+
 public class Student {
+
+    public static long id;
+
+    public static String name;
+
+    public static ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        name = studentName;
+        id = studentID;
+        grades = new ArrayList<Integer>();
+    }
+
+    // returns the student's id
+    public long getId(){
+        return id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for (int grade : grades) {
+            sum = sum + grade;
+        }
+        return sum / grades.size();
+    }
+
 }
